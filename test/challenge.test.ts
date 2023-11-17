f998a724b0df436c77050bc4438cec8bf5c902c6
allentsangdev <allentsang04@gmail.com>
2023-11-17 12:24:10 -0500

    refactor tests

--- test/challenge.test.ts
+++ test/challenge.test.ts
@@ -1,6 +1,5 @@
 import { Ludex } from "@ludex-labs/ludex-sdk-js";
 import { expect } from "chai";
-import { ZodError } from "zod";
 import dotenv from "dotenv";
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
@@ -28,54 +27,75 @@ describe("Challenge Class Error Handling Tests", () => {
   //   });
 
   /*-------------------------------------------- getChallenge -------------------------------------------- */
-  it("getChallenge : it should return a zod error", async function () {
+  it("getChallenge : it should return a zod error on the challengeId", async function () {
     this.timeout(defaultTestTimeout);
-    try {
-      // @ts-ignore
-      await ludexChallengeAPI.getChallenge("abc");
-    } catch (error: any) {
-      expect(error.errors[0].message).to.be.eq(
-        "Expected number, received string"
-      );
-    }
+    // @ts-ignore
+    await expect(ludexChallengeAPI.getChallenge('abc')).to.eventually.be.rejected;
+    // @ts-ignore
+    const error = await ludexChallengeAPI.getChallenge('abc').catch((err) => err);
+    expect(error.name).to.be.eq("ZodError")
+    expect(error.errors[0].message).to.be.eq("Expected number, received string");
   });
 
-  it("getChallenge : it should return a Axios error", async function () {
+  it("getChallenge : it should return a AxiosError: Challenge not found", async function () {
     this.timeout(defaultTestTimeout);
-    try {
-      await ludexChallengeAPI.getChallenge(99999);
-    } catch (error: any) {
-      expect(error.response.data.message).to.be.eq("Challenge not found");
-    }
+    // @ts-ignore
+    await expect(ludexChallengeAPI.getChallenge(999999)).to.eventually.be.rejected;
+    // @ts-ignore
+    const error = await ludexChallengeAPI.getChallenge(999999).catch((err) => err);
+    expect(error.name).to.be.eq("AxiosError")
+    expect(error.response.data.message).to.be.eq("Challenge not found");
   });
 
   /*-------------------------------------------- getChallenges -------------------------------------------- */
-  it("getChallenge : it should return a zod error", async function () {
+  it("getChallenges : it should return a zod error on every field of the filter object", async function () {
     this.timeout(defaultTestTimeout);
-    try {
-      const filter = {
-        payoutId: 'abc',
-        environment: 'sandbox'
-      }
-      // @ts-ignore
-      await ludexChallengeAPI.getChallenges(filter);
-    } catch (error: any) {
-      console.log(error)
-      expect(error.errors[0].message).to.be.eq(
-        "Expected number, received string"
-      );
-      expect(error.errors[1].message).to.be.eq(
-        "Invalid enum value. Expected 'MAINNET' | 'DEVNET', received 'sandbox'");
+    const filter = {
+      payoutId: 'abc',
+      environment: 'sandbox',
+      state: 'OK',
+      type: 'SNFT',
+      chain: 'ETH',
+      page: 123,
+      pageLimit: 'abc'
     }
+    // @ts-ignore
+    await expect(ludexChallengeAPI.getChallenges(filter)).to.eventually.be.rejected;
+    // @ts-ignore
+    const error = await ludexChallengeAPI.getChallenges(filter).catch((err) => err);
+    expect(error.name).to.be.eq("ZodError")
+    expect(error.errors[0].message).to.be.eq("Expected number, received string");
+    expect(error.errors[1].message).to.be.eq("Invalid enum value. Expected 'MAINNET' | 'DEVNET', received 'sandbox'");
+    expect(error.errors[2].message).to.be.eq("Invalid enum value. Expected 'CREATING' | 'CREATED' | 'LOCKING' | 'LOCKED' | 'CANCELING' | 'CANCELED' | 'RESOLVING' | 'RESOLVED' | 'VERIFYING_CANCEL' | 'VERIFYING_RESOLVE' | 'VERIFIED_CANCEL' | 'VERIFIED_RESOLVE' | 'REDEEMING_CANCEL' | 'REDEEMING_RESOLVE' | 'REDEEMED_CANCEL' | 'REDEEMED_RESOLVE', received 'OK'");
+    expect(error.errors[3].message).to.be.eq("Invalid enum value. Expected 'NATIVE' | 'FT' | 'NFT', received 'SNFT'");
+    expect(error.errors[4].message).to.be.eq("Invalid enum value. Expected 'SOLANA' | 'AVALANCHE', received 'ETH'");
+    expect(error.errors[5].message).to.be.eq("Expected string, received number");
+    expect(error.errors[6].message).to.be.eq("Expected number, received string");
   });
   
-  // it("getChallenges : it should return a Axios error", async function () {
-  //   this.timeout(defaultTestTimeout);
-  //   try {
-  //     await ludexChallengeAPI.getChallenge(99999);
-  //   } catch (error: any) {
-  //     expect(error.response.data.message).to.be.eq("Challenge not found");
-  //   }
-  // });
+  
+  it("getChallenges : it should return a Axios error: Page limit too high", async function () {
+    this.timeout(defaultTestTimeout);
+    const filter = {
+      pageLimit: 999999,
+    }
+    await expect(ludexChallengeAPI.getChallenges(filter)).to.eventually.be.rejected;
+    const error = await ludexChallengeAPI.getChallenges(filter).catch((err) => err);
+    expect(error.name).to.be.eq("AxiosError")
+    expect(error.response.data.message).to.be.eq('Page limit too high')
+  });
+
+  it("getChallenges : it should not return error with no params", async function () {
+    this.timeout(defaultTestTimeout);
+    // @todo : remove ts-ignore after merging the SDK filter fix PR
+    // @ts-ignore
+    const response = await ludexChallengeAPI.getChallenges()
+    console.log(response.data)
+    expect(response).to.be.an('object')
+    expect(response.data).to.have.property('challenges')
+    expect(response.data).to.have.property('remainingRecords')
+  });
+
+  /*-------------------------------------------- createChallenges -------------------------------------------- */
 
 });
