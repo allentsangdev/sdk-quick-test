46716f0c6632960d324b7bb28e3966f45e9d7e47
allentsangdev <allentsang04@gmail.com>
2023-11-17 19:22:52 -0500

    add apiClient tests

--- test/apiClient.test.ts
+++ test/apiClient.test.ts
@@ -1,24 +1,26 @@
-import { Ludex, Chain, RedeemType } from "@ludex-labs/ludex-sdk-js";
+import { Ludex } from "@ludex-labs/ludex-sdk-js";
 import { expect } from "chai";
 import dotenv from "dotenv";
-import chai from "chai";
-import chaiAsPromised from "chai-as-promised";
-chai.use(chaiAsPromised);
 dotenv.config();
 
 describe("apiClient Class Error Handling Tests", () => {
   const defaultTestTimeout = 30000;
+
+  const createApiClient = () => {
+    // @ts-ignore
+    return new Ludex.ClientScoped(123,{baseUrl: 123,})
+  }
   
   before(async () => {});
   /*-------------------------------------------- apiClient -------------------------------------------- */
   it("apiClient : it should return a zod error apikey and baseUrl", async function () {
     this.timeout(defaultTestTimeout);
-    // @ts-ignore
-    await expect( () => {new Ludex.ClientScoped(123,{baseUrl: 123,})}).to.eventually.be.rejected;
-    // @ts-ignore
-    const error = await ludexVaultAPI.getVault('abc').catch((err) => err);
-    expect(error.name).to.be.eq("ZodError")
-    expect(error.errors[0].message).to.be.eq("Invalid enum value. Expected 'SOLANA' | 'AVALANCHE', received 'abc'");
+    try {
+        createApiClient()
+    } catch(error : any) {
+        expect(error.name).to.be.eq("ZodError")
+        expect(error.errors[0].message).to.be.eq("Expected string, received number");
+    }
   });
 
 });
